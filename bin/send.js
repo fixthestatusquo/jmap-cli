54d570cf7f7e09fbf7d81aa5467c5d3b99d2aee9
=Xavier <github@tttp.eu>
2025-07-30 18:48:15 +0200

    WIP

---
+++ bin/send.js
@@ -0,0 +1,20 @@
+#!/usr/bin/env node
+
+import minimist from 'minimist';
+import dotenv from 'dotenv';
+import { getAccessToken } from '../lib/auth.js';
+import { sendEmail } from '../lib/jmap.js';
+
+dotenv.config();
+
+const args = minimist(process.argv.slice(2));
+const { from = process.env.EMAIL_FROM, to, subject, text } = args;
+
+if (!from || !to || !subject || !text) {
+  console.error('Usage: send-email --from me@example.com --to someone@example.com --subject "Hello" --text "Body"');
+  process.exit(1);
+}
+
+const token = await getAccessToken();
+await sendEmail(token, { from, to, subject, text });
+
